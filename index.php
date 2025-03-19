06247266e00499fb568a4cb8c3cec99d21ca7585
sonykhan05 <usmanblogging47@gmail.com>
2025-03-20 03:25:09 +0500

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// This file allows the URL shortener to work on PHP-enabled hosting
-// Simply uploading these files to most web hosts should work
-
-// Just include the HTML file
-include 'index.html';
-?> 
